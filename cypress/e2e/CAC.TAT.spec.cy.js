f3e3cd7e461b1ad584feb85e910b8c23ecc9a506
gabidevs <gabrieldev73@gmail.com>
2023-05-22 16:53:53 -0300

    Primeiros exercícios

--- cypress/e2e/CAC.TAT.spec.cy.js
+++ cypress/e2e/CAC.TAT.spec.cy.js
@@ -1,13 +1,41 @@
 //Automação básica de cypress, curso TAT
 
-describe('template spec', () => {
+describe('Hooks', () => {
+
+  beforeEach(function(){
+    cy.visit('../src/index.html')
+      .title()
+      .should('eq','Central de Atendimento ao Cliente TAT')
+  })
+
+  it('passes', () => {
+    cy.get('#firstName')
+      .should('be.visible')
+      .type('Gabriel')
+      .get('#lastName')
+      .should('be.visible')
+      .type('Oliveira')
+      .get('#email')
+      .type('gabrieldev73@gmail.com')
+      .get('#open-text-area')
+      .type('Teste de envio de mensagem a central de atendimento ao cliente')
+      .get('button[type="submit"]')
+      .click()
+      .get('.success')
+      .should('be.visible')
+})
+})
+
+/* describe('template spec', () => {
   it('passes', () => {
     cy.visit('../src/index.html')
     cy.title().should('eq','Central de Atendimento ao Cliente TAT')
     cy.get('#firstName')
+      .should('be.visible')
       .type('Gabriel')
       .get('#lastName')
+      .should('be.visible')
       .type('Oliveira')
       
   })
-})
+}) */
